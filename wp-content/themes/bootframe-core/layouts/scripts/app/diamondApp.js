21be74674a9c382aa85e2d986db4b82a42c8b8eb
Axel Johansson <axel.johansson@accigo.se>
2016-03-02 09:34:23 +0100

    removed old theme.

--- wp-content/themes/bootframe-core/layouts/scripts/app/diamondApp.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-var diamondApp = angular.module('diamondApp', []);
-// BadController will fail to instantiate, due to relying on automatic function annotation,
-// rather than an explicit annotation
-
-//used for calls to api
-var baseUrl =  location.href.substr(0, location.href.length - 3);
