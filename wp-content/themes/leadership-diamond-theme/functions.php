4927dfd5d32f6ef6d5735f368557883bdab99eea
Jens Klemets <jens.klemets@accigo.se>
2016-06-28 09:27:14 +0200

    big check in

--- wp-content/themes/leadership-diamond-theme/functions.php
+++ wp-content/themes/leadership-diamond-theme/functions.php
@@ -4,29 +4,24 @@ $version = '0.81';
 
 function my_theme_enqueue_styles() {
     global $version;
-    /* BOOTSTRAP */
-    wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.css', array(), '1.0.0' );
+    
+    /* CHILD AND PARENT STYLE */
     $parent_style = 'parent-style';
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), $version, true);
-  /* BOOTSTRAP */
-  wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.css', array(), '1.0.0' );
-
+    
+    /* BOOTSTRAP */
+    wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.css', array(), '1.0.0' );
     
   /* HOVER */
   wp_enqueue_style( 'hover', get_stylesheet_directory_uri() . '/css/hover-min.css', array(), '1.0.0' );
 
-  /* SCRIPT AND STYLES VERSION */
-  $parent_style = 'parent-style';
-  $version = '0.9';
-  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), $version, true);
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 function my_theme_enqueue_scripts() {
     global $version;
-    /* Angular */
+    /* ANGULAR */
     wp_enqueue_script('angular', get_stylesheet_directory_uri() . '/js/assets/angular.js', array(), null, true);
     wp_enqueue_script('angular_animate', get_stylesheet_directory_uri() . '/js/assets/angular-animate.js', array(), null, true);
     wp_enqueue_script('angular_touch', get_stylesheet_directory_uri() . '/js/assets/angular-touch.js', array(), null, true);
@@ -38,9 +33,10 @@ function my_theme_enqueue_scripts() {
      wp_enqueue_script('materialize_js', get_stylesheet_directory_uri() . '/js/assets/materialize.min.js', array(), null, true);
 
     /* CUSTOM SCRIPTS */
-    /* Diamond App */
+    /* DIAMOND APP */
     wp_enqueue_script('diamond_app', get_stylesheet_directory_uri() . '/js/app/diamondApp.js', array(), $version, true);
     wp_enqueue_script('start_ctrl', get_stylesheet_directory_uri() . '/js/app/controllers/startCtrl.js', array(), $version, true);
+    
 
 wp_enqueue_script('start_svc', get_stylesheet_directory_uri() . '/js/app/services/startSvc.js', array(), $version, true);
 }
--- wp-content/themes/leadership-diamond-theme/js/app/diamondApp.js
+++ wp-content/themes/leadership-diamond-theme/js/app/diamondApp.js
@@ -2,39 +2,45 @@
 
 var diamondApp = angular.module('diamondApp', ['ngAnimate', 'ngTouch', 'ui.bootstrap'])
 
-.run(['$anchorScroll', function($anchorScroll) {
-  $anchorScroll.yOffset = 150;    // always scroll by 50 extra pixels
+.run(['$anchorScroll', function ($anchorScroll) {
+    $anchorScroll.yOffset = 150; // always scroll by 50 extra pixels
 }]);
 
 // Global variabels
 var environments = {
-	"dev": "http://localhost",
-	"prod": "http://www.leadershipdiamond.com"
+    "dev": "http://localhost"
+    , "prod": "http://www.leadershipdiamond.com"
 };
 
 var currentEnvironment = "";
 //Setting the current environment
-angular.forEach(environments, function(env){
-	if (env === location.origin) {
-		currentEnvironment = env;
-	};
+angular.forEach(environments, function (env) {
+    if (env === location.origin) {
+        currentEnvironment = env;
+    };
 });
 
 //Adding a baseUrl, used in Angular services
 if (currentEnvironment === environments.dev) {
-	var baseUrl = currentEnvironment.concat('/leadershipdiamond');
+    var baseUrl = currentEnvironment.concat('/leadershipdiamond');
 } else {
-	baseUrl = currentEnvironment;
+    baseUrl = currentEnvironment;
 }
 
 //Setting current language depending on url (Default = Swedish)
-var currentLanguage = {"name": "Svenska", "url": baseUrl.concat("/sv")};
-if(location.href.search("en") > -1){
-    var currentLanguage = {"name": "English", "url": baseUrl.concat("/en")};
+var currentLanguage = {
+    "name": "Svenska"
+    , "url": baseUrl.concat("/sv")
+};
+if (location.href.search("en") > -1) {
+    var currentLanguage = {
+        "name": "English"
+        , "url": baseUrl.concat("/en")
+    };
 }
 
 jQuery("#page").attr("ng-controller", "startCtrl");
 
 jQuery(document).ready(function () {
-    angular.bootstrap(document.getElementById("page"), ['diamondApp']); 
+    angular.bootstrap(document.getElementById("page"), ['diamondApp']);
 });
